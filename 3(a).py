8a8c28259663cba94b861f07ea2142ebb450b0bb
Shrinivas Sondur <shrinivassondur03@gmail.com>
2025-03-29 23:38:17 +0530

    Add files via upload

---
+++ 3(a).py
@@ -0,0 +1,7 @@
+import numpy as np
+list_data=[1,2,3,4,5]
+array_list=np.array(list_data,dtype=float)
+print("array from list",array_list)
+tuple_data=(6,7,8,9,10)
+array_tuple=np.array(tuple_data,dtype=float)
+print("Array from tuple",array_tuple)
