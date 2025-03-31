445fe53b992a61a9a5870a27636af25882c46639
Shrinivas Sondur <shrinivassondur03@gmail.com>
2025-03-31 19:46:23 +0530

    Add files via upload

---
+++ 3(c).py
@@ -0,0 +1,6 @@
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print("minimum value",np.min(arr))
+print("maximum value",np.max(arr))
+print("sum of element",np.sum(arr))
+print("Cummulative",np.cumsum(arr))
